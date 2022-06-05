87ba9f8fc1041b6cad75869cc6cd47e2093485ae
shusain24 <mdsaddam121296@gmail.com>
2022-06-05 19:46:28 +0530

    Using of Typewritter in Homepage

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Home.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useTypewriter } from 'react-simple-typewriter';
+// import {TypeWriterEffect} from 'react-typewriter-effect';
+import './Home.css';
+import logo from './loga.png';
 
 export default function Home() {
+  const {text} = useTypewriter({
+    words: [ "I am Saddam Husain.","I am a Software Engineer(Full Stack Developer).","have an one year of exprience .",],
+    loop: 0, 
+    typeSpeed: 70,
+    deleteSpeed: 50,
+    delaySpeed: 1000
+    })
    
   return (
    <div className="container">
      <div className="row">
-        <div className="col-sm-4">
+        <div className="col-sm-5"> 
           <div className="card cardStyle">
+            <img className='imageStyle' src={logo} alt='myLogo'/>
             <div className="card-body">
-              <p className='headingStyle'>Hi,</p>
-              <p>I'm  </p>
+              <p className='headingStyle'>Hey, <span> Good Morning...!!</span></p>
+              <p className='headingStyle1'>{text}</p>
               {/* <Link to="#" className="btn btn-primary">Go somewhere</Link> */}
             </div>
           </div>
         </div>
-      <div className="col-sm-8">
+      <div className="col-sm-7">
         <div className="card ">
           <div className="card-body">
             <h5 className="card-title">Special title treatment</h5>
